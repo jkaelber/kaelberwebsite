6d8dd0771bee28904e26649776f6eca61b56eefb
jkaelber <jtk52@cornell.edu>
2023-07-23 16:22:38 -0400

    skip eid 2

--- _cite/plugins/orcid.py
+++ _cite/plugins/orcid.py
@@ -35,19 +35,18 @@ def main(entry):
     # go through response structure and pull out ids e.g. doi:1234/56789
     for work in response:
         for id in work["external-ids"]["external-id"]:
-            if id["external-id-type"] == "eid":
-                pass
-            # get id and id-type from response
-            id_type = id["external-id-type"]
-            id_value = id["external-id-value"]
+            if id["external-id-type"] != "eid":
+                # get id and id-type from response
+                id_type = id["external-id-type"]
+                id_value = id["external-id-value"]
 
-            # create source
-            source = {"id": f"{id_type}:{id_value}"}
+                # create source
+                source = {"id": f"{id_type}:{id_value}"}
 
-            # copy fields from entry to source
-            source.update(entry)
+                # copy fields from entry to source
+                source.update(entry)
 
-            # add source to list
-            sources.append(source)
+                # add source to list
+                sources.append(source)
 
     return sources
